0933a0e3be04e53eb2898ccb2ea8164a8353de54
Amit <ambedkaramit176@gmail.com>
2024-04-06 19:21:06 +0530

    add empty file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className=" text-black bg-red-200"> hello Blogs App </h2>
+    </>
+  );
+}
+
+export default App;
